ab4ae58c1f7870c574f7466055f8938f45b7265a
Pedro Henrique Milan <pedro.milan9@gmail.com>
2022-05-12 19:02:11 -0300

    Netflix Clone

---
+++ src/components/Row.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { getMovies } from "../api";
+import "./Row.css";
+
+const imageHost = "https://image.tmdb.org/t/p/original";
+
+const Row = ({ title, path }) => {
+  const [movies, setMovies] = React.useState([]);
+  const fetchMovies = async (_path) => {
+    try {
+      const data = await getMovies(_path);
+      console.log("data", data);
+      setMovies(data?.results);
+    } catch (error) {
+      console.log("fetchMovies error:", error);
+    }
+  };
+  React.useEffect(() => {
+    fetchMovies(path);
+  }, [path]);
+
+  return (
+    <div className="row-container">
+      <h2 className="row-header">{title}</h2>
+      <div className="row-cards">
+        {movies?.map((movie) => {
+          return (
+            <img
+              className="movie-card"
+              key={movie.id}
+              src={`${imageHost}${movie.poster_path}`}
+              alt={movie.name}
+            ></img>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export { Row };
